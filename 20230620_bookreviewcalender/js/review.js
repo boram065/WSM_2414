2387ebbabcc058d8e8729ec6393e6463e0955798
boram065 <boram4784@gmail.com>
2023-08-08 12:15:59 +0900

    Get URL book parameter -> string -> number -> bookData -> HTML, GetNow

--- 20230620_bookreviewcalender/js/review.js
+++ 20230620_bookreviewcalender/js/review.js
@@ -1,9 +1,60 @@
 // URL book에 해당하는 parameter 값 읽어보자(string)
-var url = window.location.href; //현재 페이지 주소 가져오기
-var book = url.substring(url.indexOf("=") + 1); //페이지 주소에서 '=' 다음부터 출력하기
-console.log(`book : ${book}`);
+// var url = window.location.href; //현재 페이지 주소 가져오기
+// var book = url.substring(url.indexOf("=") + 1); //페이지 주소에서 '=' 다음부터 출력하기
+let url = new URL(location.href);
+let book = url.searchParams.get("book");
+// book이 null이면 책장으로 가자
+if(book === null) {
+    location.href = 'index.html';
+    // window.open('index.html', "_top"); // _top : 현재 창에 열기
+}
+console.log(`book : ${book} type : ${typeof book}`); // "3" string
 
-// book 값(string) -> bookId(number) 변환하자
+// book 값(string) -> bookId(number) 변환하자 = "3" -> 3
+let bookId = Number(book);
+console.log(`bookId : ${bookId} type : ${typeof bookId}`); // 3 number
+
 // books 를 돌면서 bookId(number)와 같은 id값의 책을 가져오자
+let bookData;
+for (let bookOne of books) {
+    if(bookId === bookOne.id) {     // === : 값과 자료형 타입 둘 다 맞아야 True
+        bookData = bookOne;
+        break;
+    }
+}
 
-// console.log("book : ");
+// 제목 보여주자
+let title = bookData.title;
+console.log(title);
+
+let author = bookData.author;
+let publisher = bookData.publisher;
+let bookImg = bookData.img;
+console.log(title, author, publisher, bookImg);
+
+// HTML 요소를 js 변수로 가져오기
+// js 변수.innerHTML
+const bookImgDiv = document.getElementsByClassName("book-image")[0];
+bookImgDiv.innerHTML = `<img src = "${bookImg}" />`; // div에 이미지 넣기
+
+const titleDiv = document.getElementsByClassName("title")[1];
+titleDiv.innerHTML = title;
+
+const authorDiv = document.getElementsByClassName("author")[1];
+authorDiv.innerHTML = author;
+
+const publisherDiv = document.getElementsByClassName("publisher")[1];
+publisherDiv.innerHTML = publisher;
+
+// 읽은 날짜 : 임시로 오늘의 날짜 표시하자
+// 오늘의 날짜 구하자
+let now = new Date();
+let year = now.getFullYear();
+let month = now.getMonth() + 1;
+let date = now.getDate();
+// 사람이 알아보는 형식으로 바꾸자
+let dateString = `${year}년 ${month}월 ${date}일`;
+// HTML -> js
+const readDateDiv = document.getElementsByClassName("read-date")[1];
+// 오늘의 날짜 값 넣자
+readDateDiv.innerHTML = dateString;
