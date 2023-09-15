14daeadb98acb4f35040bb0521e17c90fc316bb0
boram065 <boram4784@gmail.com>
2023-09-15 09:49:12 +0900

    달력에 날짜 나오게 -> html appendChild()

--- 20230620_bookreviewcalender/js/calendar.js
+++ 20230620_bookreviewcalender/js/calendar.js
@@ -2,25 +2,39 @@ var now = new Date();
 var year = now.getFullYear();
 var month = now.getMonth() + 1;
 
+const datesContainerDiv = document.getElementsByClassName("dates container")[0];
+
 // < : 이전 달
-month -= 1;
+// month -= 1;
 // > : 이후 달
-month += 1;
+// month += 1;
 
 const setCalendar = (year, month) => {
-    console.log(year, month);
+    // 현재 월 제목에 표시하자
+    // html -> js
+    var titleMonthDiv = document.getElementsByClassName("month")[0];
+
+    //= `${month}월`;
+    titleMonthDiv.innerHTML = `${month}월`;
+
+    // 1~해당 월의 마직막 날짜까지 날짜 만들자
+    // 해당 월의 마지막 날짜
+    var thisMonthLastDate = new Date(year, month+1-1, 0).getDate(); // 10월의 전날 : 9월 30일
+    for(let date = 1; date <= thisMonthLastDate; date++) {
+        let dateItemDiv = document.createElement("div"); // <div></div>
+        dateItemDiv.classList.add("date"); // <div class="date"></div>
+        dateItemDiv.classList.add("item"); // <div class="item"></div>
+        dateItemDiv.innerHTML = date;
+
+        // dates.container에게 자식으로 붙이자
+        datesContainerDiv.appendChild(dateItemDiv);
+    }
+
+    // 해당 월의 1일은 무슨 요일인가?
+    var thisMonthFirstDay = new Date(year, month-1, 1).getDay();
+
 }
 setCalendar(year, month);
-
-
-// 해당 월의 마지막 날짜
-// 9월 마지막 날짜
-var thisMonthLastDate = new Date(year, month+1-1, 0).getDate(); // 10월의 전날 : 9월 30일
-
-// 해당 월의 1일은 무슨 요일인가?
-var thisMonthFirstDay = new Date(year, month-1, 1).getDay();
-
-console.log(thisMonthLastDate, thisMonthFirstDay);
 
 
 // switch(thisMonthFirstDay) {
