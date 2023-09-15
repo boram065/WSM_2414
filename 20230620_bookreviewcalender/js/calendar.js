7ce34f556288017fea9c2acc82d8fd0af383ba0b
boram065 <boram4784@gmail.com>
2023-09-15 14:00:14 +0900

    2023-09-15

--- 20230620_bookreviewcalender/js/calendar.js
+++ 20230620_bookreviewcalender/js/calendar.js
@@ -4,11 +4,6 @@ var month = now.getMonth() + 1;
 
 const datesContainerDiv = document.getElementsByClassName("dates container")[0];
 
-// < : 이전 달
-// month -= 1;
-// > : 이후 달
-// month += 1;
-
 const setCalendar = (year, month) => {
     // 현재 월 제목에 표시하자
     // html -> js
@@ -30,21 +25,52 @@ const setCalendar = (year, month) => {
         datesContainerDiv.appendChild(dateItemDiv);
     }
 
-    // 해당 월의 1일은 무슨 요일인가?
-    var thisMonthFirstDay = new Date(year, month-1, 1).getDay();
+    // 1일을 해당 요일로 이동하자
+    let firstDateDiv = datesContainerDiv.querySelector(".date.item:nth-child(1)");
+    // css : .date.item:nth-child(1) {grid-column-start: 6;}
+    // 1일의 요일 구하자 year, month -> month 1일의 요일 :  9월 : 5
+    var firstDateDay = new Date(year, month-1, 1).getDay();
+    firstDateDiv.style.gridColumnStart = firstDateDay+1;
+
+    // 토요일 : 파란색 글자로
+    let saturdayFirstDate = 2;
+    let saturdayDivs = datesContainerDiv.querySelectorAll(`.date.item:nth-child(7n+${saturdayFirstDate}`);
+    for (let saturdayDiv of saturdayDivs) {
+        // .date.item:nth-child(7n+2) {color:blue;}
+        saturdayDiv.style.color = "blue";
+    }
+
+    // 일요일 : 빨간색 글자로
+    let sundayFirstDate = 3;
+    let sundayDivs = datesContainerDiv.querySelectorAll(`.date.item:nth-child(7n+${sundayFirstDate})`);
+    for (let sundayDiv of sundayDivs) {
+        // .date.item:nth-child(7n+3) {color:red;}
+        sundayDiv.style.color = "red";
+    }
 
 }
 setCalendar(year, month);
 
+// < : 이전 달
+const leftDiv = document.getElementsByClassName("left")[0];
+leftDiv.onclick = () => {
+    month--;
+    let prevMonth = new Date(year, month-1);
+    year = prevMonth.getFullYear();
+    month = prevMonth.getMonth()+1;
+    setCalendar(year, month);
+}
+// leftDiv.addEventListener("click", () => console.log("이전 달"));
 
-// switch(thisMonthFirstDay) {
-//     case 0 : thisMonthFirstDay = "일요일"; break;
-//     case 1 : thisMonthFirstDay = "월요일"; break;
-//     case 2 : thisMonthFirstDay = "화요일"; break;
-//     case 3 : thisMonthFirstDay = "수요일"; break;
-//     case 4 : thisMonthFirstDay = "목요일"; break;
-//     case 5 : thisMonthFirstDay = "금요일"; break;
-//     case 6 : thisMonthFirstDay = "토요일"; break;
-// }
+// > : 이후 달
+let rightDiv = document.getElementsByClassName("right")[0];
+rightDiv.onclick = () => {
+    month++;
+    if(month == 13) {
+        year++;
+        month = 1;
+    }
+    setCalendar(year, month);
+}
 
 // 해당 월의 마자막 요일
